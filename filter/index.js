436b9f404bf706fbfc4ca41ab0500b0413131c65
Exephium <exephium@gmail.com>
2024-08-05 22:21:50 +0300

    filter

---
+++ filter/index.js
@@ -0,0 +1,12 @@
+function filter(list, callbackFn) {
+  const arr = []
+
+  for (let i = 0; i < list.length; i++) {
+    if (callbackFn(list[i]))
+      arr.push(list[i])
+  }
+
+  return arr
+}
+
+const result = filter([12, 5, 8, 130, 44], (el) => el >= 10)
