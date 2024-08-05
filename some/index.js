5f4dc0354cfec6161e8730ceced6fc7ef12ba085
Exephium <exephium@gmail.com>
2024-08-05 22:22:15 +0300

    some

---
+++ some/index.js
@@ -0,0 +1,10 @@
+function some(list, callbackFn) {
+  for (let i = 0; i < list.length; i++) {
+    if (callbackFn(list[i]))
+      return true
+  }
+
+  return false
+}
+
+const result = some([1, 2, 3, 4, 5], (el) => el % 2 === 0)
